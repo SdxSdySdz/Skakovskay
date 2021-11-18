0c29ca24841d95b385152a7f9dfcb0087bf46273
Timur <timurok97@gmail.com>
2021-11-18 12:05:15 +0300

    Add more OOP

--- image_info.py
+++ image_info.py
@@ -4,77 +4,64 @@ import numpy as np
 import tkinter as tk
 
 
+class TK_Element:
+    @property
+    def array(self):
+        return self._array
+
+    @property
+    def array_as_string(self):
+        return str(self._array)
+
+    def __init__(self, app, array: np.ndarray):
+        self._app = app
+        self._array = array
+        self._string_var: tk.StringVar = tk.StringVar()
+        self._label: tk.Label = None
+
+        self._string_var.set(self.array_as_string)
+
+    def init_label(self, row: int, column: int):
+        self.on_init_label()
+        self._label.grid(row=row, column=column)
+
+    def on_init_label(self):
+        raise NotImplementedError()
+
+
+class ArrayElement(TK_Element):
+    def __init__(self, app, array: np.ndarray):
+        super().__init__(app, array)
+
+    def on_init_label(self):
+        self._label = tk.Label(self._app, textvariable=self._string_var)
+
+
+class ImageElement(TK_Element):
+    def __init__(self, app, img_array: np.ndarray):
+        super().__init__(app, img_array)
+
+    def on_init_label(self):
+        img = ImageTk.PhotoImage(Image.fromarray(self.array))
+        self._label = tk.Label(self._app, image=img)
+        self._label.image = img
+
+
 class ImageInfo:
-    @property
-    def PIL_image(self):
-        return self._PIL_image
-    
-    @property
-    def gray_image(self):
-        return self._gray_image
-
-    @property
-    def binary_image(self):
-        return self._binary_image
-
-    @property
-    def gray_image_as_string(self):
-        return str(self._gray_image)
-    
-    @property
-    def binary_image_as_string(self):
-        return str(self._binary_image)
-
-    @property
-    def TK_gray_string_var(self):
-        return self._TK_gray_string
-
-    @property
-    def TK_binary_string_var(self):
-        return self._TK_binary_string
-    
-    @property
-    def image_label(self):
-        return self._image_label
-    
-    @property
-    def gray_label(self):
-        return self._gray_label
-
-    @property
-    def binary_label(self):
-        return self._binary_label
-
     def __init__(self, app, image: Image, binary_threshold: float):
-        self._PIL_image = image
-
-        self._gray_image = (np.array(image) * 255).astype(int)[:, :, 0]
-        self._binary_image = (self._gray_image >= (binary_threshold * 255)).astype(int)
-
-        self._TK_gray_string = tk.StringVar()
-        self._TK_binary_string = tk.StringVar()
-
-        self._TK_gray_string.set(self.gray_image_as_string)
-        self._TK_binary_string.set(self.binary_image_as_string)
-
-        img = ImageTk.PhotoImage(Image.fromarray(self._gray_image))
-        binary_img = ImageTk.PhotoImage(Image.fromarray(self._binary_image * 255))
-        self._image_label = tk.Label(app, image=img)
-        self._binary_image_label = tk.Label(app, image=binary_img)
-        self._gray_label = tk.Label(app, textvariable=self._TK_gray_string)
-        self._binary_label = tk.Label(app, textvariable=self._TK_binary_string)
-
-        self._image_label.image = img
-        self._binary_image_label.image = binary_img
+        self._gray_image = ImageElement(app, np.array(image))
+        self._gray_matrix = ArrayElement(app, (np.array(image) * 255).astype(int)[:, :, 0])
+        self._binary_matrix = ArrayElement(app, (self._gray_matrix.array >= (binary_threshold * 255)).astype(int))
+        self._binary_image = ImageElement(app, self._binary_matrix.array * 255)
 
     def init_image_label(self, row: int, column: int):
-        self._image_label.grid(row=row, column=column)
+        self._gray_image.init_label(row, column)
 
     def init_gray_label(self, row: int, column: int):
-        self._gray_label.grid(row=row, column=column)
+        self._gray_matrix.init_label(row, column)
 
     def init_binary_label(self, row: int, column: int):
-        self._binary_label.grid(row=row, column=column)
+        self._binary_matrix.init_label(row, column)
 
     def init_binary_image_label(self, row: int, column: int):
-        self._binary_image_label.grid(row=row, column=column)
+        self._binary_image.init_label(row, column)
